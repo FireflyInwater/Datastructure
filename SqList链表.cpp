9302af09afbcfdd2e1324b91737d5ecab649ed51
xbchen <1026377981@qq.com>
2020-10-04 14:54:09 +0800

    LinkedList class cpp

--- SqList链表.cpp
+++ SqList链表.cpp
@@ -18,6 +18,41 @@ typedef struct Node{
 //把 struct Node * 定义成了类型 LinkList。这个类型是一个结构体的指针
 typedef struct Node *LinkList; /*定义链表*/
 
+/*每一个Node节点是以指针表示的*/
+
+Status CreateListHead(LinkList *L,int n){
+    //头插法创建整表；新节点始终放在前面;
+    
+    LinkList p;
+    *L = new Node; //头结点
+    (*L)->next = NULL; 
+
+    for(int i=0;i<n;i++)
+    {
+        p  = new Node;
+        p->data = rand()%100 + 1; //每一个节点的数据
+        p->next = (*L)->next;
+        (*L)->next = p;
+    }
+    return OK;
+}
+
+Status CreateListTail(LinkList *L,int n){
+    //尾插法；按节点生成顺序将每个新的节点放在表尾
+    LinkList p,t; //t是指向最后一个节点的指针
+    *L = new Node;
+    t = *L;
+    for(int i=0;i<n;i++){
+        p = new Node;
+        p->data = rand()%100 + 1;
+        p->next = NULL;
+        t->next = p;
+        t = p;
+    }
+    return OK;
+}
+
+
 Status GetElem(LinkList L,int i, ElemType *e){
     int j = 1;
     LinkList p; //声明一个Node指针
@@ -63,4 +98,28 @@ Status ListDelete(LinkList *L, int i,ElemType *e){
     p->next = q->next;
     free(q); //释放内存
     return OK;   
+}
+
+Status ClearList(LinkList *L)
+{
+    LinkList p,q;
+    p = (*L)->next; //指向表头
+    while (p)
+    {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    (*L)->next = NULL;
+    return OK;
+}
+
+
+int main(){
+    LinkList L1;
+    CreateListTail(&L1,10);
+    for(LinkList p = L1;p->next!=NULL;p++){
+        std::cout<<p->data<<std::endl;
+    }
+    return 0;   
 }
